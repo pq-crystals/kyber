516841f2aa3c36318c6e17d606cf122ee25d5db4
Peter Schwabe <peter@cryptojedi.org>
2017-11-24 19:35:39 +0100

    Fixed consts.c (hopefully now fully supporting clang and OSX)

--- avx2/consts.c
+++ avx2/consts.c
@@ -6,13 +6,13 @@
 #define MONT 4088U
 #define F ((((uint32_t)MONT*MONT % Q) * (Q-1) % Q) * ((Q-1) >> 8) % Q)
 
-const uint16_t _16xqinv[16] __attribute__((aligned(32))) = {57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857};
-const uint16_t _16xq[16] __attribute__((aligned(32))) = {Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q};
-const uint16_t _16x4q[16] __attribute__((aligned(32))) = {4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q};
-const uint16_t _16x2q[16] __attribute__((aligned(32))) = {2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q};
-const uint16_t _low_mask[16] __attribute__((aligned(32))) = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
-const  uint8_t _vpshufb_idx[32] __attribute__((aligned(32))) = {2, 3, 0, 1, 6, 7, 4, 5, 10, 11, 8, 9, 14, 15, 12, 13, 2, 3, 0, 1, 6, 7, 4, 5, 10, 11, 8, 9, 14, 15, 12, 13};
-const uint16_t _f[16] __attribute__((aligned(32))) = {F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F};
+const uint16_t _16xqinv[16] asm ("_16xqinv") __attribute__((aligned(32))) = {57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857, 57857};
+const uint16_t _16xq[16] asm ("_16xq") __attribute__((aligned(32))) = {Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q, Q};
+const uint16_t _16x4q[16] asm ("_16x4q") __attribute__((aligned(32))) = {4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q, 4*Q};
+const uint16_t _16x2q[16] asm ("_16x2q") __attribute__((aligned(32))) = {2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q, 2*Q};
+const uint16_t _low_mask[16] asm ("_low_mask") __attribute__((aligned(32))) = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+const  uint8_t _vpshufb_idx[32] asm ("_vpshufb_idx") __attribute__((aligned(32))) = {2, 3, 0, 1, 6, 7, 4, 5, 10, 11, 8, 9, 14, 15, 12, 13, 2, 3, 0, 1, 6, 7, 4, 5, 10, 11, 8, 9, 14, 15, 12, 13};
+const uint16_t _f[16] asm("_f") __attribute__((aligned(32))) = {F, F, F, F, F, F, F, F, F, F, F, F, F, F, F, F};
 const uint32_t _mask11[8] asm ("_mask11") __attribute__((aligned(32))) = {0x11111111,0x11111111,0x11111111,0x11111111,0x11111111,0x11111111,0x11111111,0x11111111};
 const uint32_t _mask0f[8] asm ("_mask0f") __attribute__((aligned(32))) = {0x0f0f0f0f,0x0f0f0f0f,0x0f0f0f0f,0x0f0f0f0f,0x0f0f0f0f,0x0f0f0f0f,0x0f0f0f0f,0x0f0f0f0f};
 const uint16_t _lowdword[16] asm ("_lowdword") __attribute__((aligned(32))) = {0xffff, 0x0, 0xffff, 0x0, 0xffff, 0x0, 0xffff, 0x0, 0xffff, 0x0, 0xffff, 0x0, 0xffff, 0x0, 0xffff, 0x0};
