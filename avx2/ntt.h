7cbb868bcea85360e74c9bcb5fbe3b82bb02515e
Tancrède Lepoint <tancrede.lepoint@gmail.com>
2017-11-25 10:53:06 +0100

    Fix to compile on OSX

--- avx2/ntt.h
+++ avx2/ntt.h
@@ -6,7 +6,7 @@
 extern const uint16_t zetas_exp[];
 extern const uint16_t zetas_inv_exp[];
 
-void ntt(uint16_t *inout, const uint16_t* zetas);
-void invntt(uint16_t *inout, const uint16_t* omegas);
+void ntt(uint16_t *inout, const uint16_t* zetas) asm("ntt");
+void invntt(uint16_t *inout, const uint16_t* omegas) asm("invntt");
 
 #endif
