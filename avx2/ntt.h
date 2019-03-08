ee582fe76326b2a1f07ba007fe667dccdda8f1f2
Gregor Seiler <gseiler@posteo.de>
2019-03-08 16:27:19 +0100

    unsigned zetas

--- avx2/ntt.h
+++ avx2/ntt.h
@@ -3,16 +3,16 @@
 
 #include <stdint.h>
 
-extern int16_t zetas_exp[396];
-extern int16_t zetas_inv_exp[396];
+extern uint16_t zetas_exp[396];
+extern uint16_t zetas_inv_exp[396];
 
-void ntt_level0_avx(int16_t *r, const int16_t *zetas);
-void ntt_levels1t6_avx(int16_t *r, const int16_t *zetas);
-void invntt_levels0t5_avx(int16_t *r, const int16_t *zetas);
-void invntt_level6_avx(int16_t *r, const int16_t *zetas);
+void ntt_level0_avx(int16_t *r, const uint16_t *zetas);
+void ntt_levels1t6_avx(int16_t *r, const uint16_t *zetas);
+void invntt_levels0t5_avx(int16_t *r, const uint16_t *zetas);
+void invntt_level6_avx(int16_t *r, const uint16_t *zetas);
 void nttpack_avx(int16_t *r);
 void nttunpack_avx(int16_t *r);
-void basemul_avx(int16_t *r, const int16_t *a, const int16_t *b, int16_t *zeta);
-void basemul_acc_avx(int16_t *r, const int16_t *a, const int16_t *b, int16_t *zeta);
+void basemul_avx(int16_t *r, const int16_t *a, const int16_t *b, uint16_t *zeta);
+void basemul_acc_avx(int16_t *r, const int16_t *a, const int16_t *b, uint16_t *zeta);
 
 #endif
