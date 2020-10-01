602eb33b375975dac644417887bd192a81ea2b30
Peter Schwabe <peter@cryptojedi.org>
2020-10-01 19:21:43 +0200

    Removed boolean operation in arithmetic statement (see https://github.com/PQClean/PQClean/pull/325#issue-486686614)

--- avx2/verify.c
+++ avx2/verify.c
@@ -27,7 +27,7 @@ int verify(const uint8_t *a, const uint8_t *b, size_t len)
     avec = _mm256_xor_si256(avec, bvec);
     cvec = _mm256_or_si256(cvec, avec);
   }
-  r = !_mm256_testz_si256(cvec,cvec);
+  r = 1-_mm256_testz_si256(cvec,cvec);
 
   if(pos < len) {
     avec = _mm256_loadu_si256((__m256i *)&a[pos]);
