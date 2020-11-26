f89320dbfcf81131d9728af6b888c68814a00be8
Gregor Seiler <gseiler@posteo.de>
2020-11-26 10:23:35 +0100

    remove csubq_avx

--- avx2/reduce.h
+++ avx2/reduce.h
@@ -6,8 +6,6 @@
 
 #define reduce_avx KYBER_NAMESPACE(_reduce_avx)
 void reduce_avx(__m256i *r, const __m256i *qdata);
-#define csubq_avx KYBER_NAMESPACE(_csubq_avx)
-void csubq_avx(__m256i *r, const __m256i *qdata);
 #define tomont_avx KYBER_NAMESPACE(_tomont_avx)
 void tomont_avx(__m256i *r, const __m256i *qdata);
 
