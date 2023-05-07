ef21083bfdf6a2104eeb5f7200f85b5af1a381c7
Peter Schwabe <peter@cryptojedi.org>
2023-05-07 15:15:10 +0200

    Fixed CC to clang in runtests; some versions of gcc seem to cause problems with address sanitizer

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
+nproc="${nproc:-2}"
 
 ARCH="${ARCH:-amd64}"
 ARCH="${TRAVIS_CPU_ARCH:-$ARCH}"
@@ -9,16 +10,19 @@ else
   DIRS="ref"
 fi
 
-#if [ "$ARCH" = "amd64" -o "$ARCH" = "arm64" ]; then
-#  export CFLAGS="-fsanitize=address,undefined ${CFLAGS}"
-#fi
+if [ "$ARCH" = "amd64" -o "$ARCH" = "arm64" ]; then
+  export CC=/usr/bin/clang 
+  export CFLAGS="-fsanitize=undefined,address ${CFLAGS}"
+fi
 
 for dir in $DIRS; do
+  make -j$(nproc) -C $dir clean
   make -j$(nproc) -C $dir
   for alg in 512 768 1024; do
     #valgrind --vex-guest-max-insns=25 ./$dir/test_kyber$alg
     ./$dir/test/test_kyber$alg &
     PID1=$!
+    echo testvec$alg
     ./$dir/test/test_vectors$alg > tvecs$alg &
     PID2=$!
     wait $PID1 $PID2
